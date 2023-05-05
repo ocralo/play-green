b135332398e29b7691c6219915ac874a5f8aa941
ocralo <ocralo@gmail.com>
2023-05-05 01:47:29 -0500

    feat: create private route

---
+++ src/Routes/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+import {IPrivateRoute} from './interfaces'
+
+export default function PrivateRoute({
+  isAllowed,
+  redirectTo = '/login',
+}: IPrivateRoute.Props): React.ReactElement | JSX.Element {
+  if (!isAllowed) {
+    return <Navigate to={redirectTo} replace />
+  }
+
+  return <Outlet />
+}
