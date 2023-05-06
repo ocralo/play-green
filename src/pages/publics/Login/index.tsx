868547ac73bb4959ba1051ae2c348108a4f40fef
ocralo <ocralo@gmail.com>
2023-05-06 03:57:49 -0500

    feat: add auth context and functionality

--- src/pages/publics/Login/index.tsx
+++ src/pages/publics/Login/index.tsx
@@ -1,3 +1,47 @@
+import {useState} from 'react'
+import {useAuth} from '@context/AuthContext/hooks'
+import {useNavigate} from 'react-router-dom'
+
 export default function Login() {
-  return <div>Login</div>
+  const navigate = useNavigate()
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  const {login, error, logout} = useAuth()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault()
+    await login(user.email, user.password)
+    navigate('/home')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='email'>Email</label>
+      <input type='email' name='email' id='email' onChange={handleChange} />
+      <label htmlFor='password'>Password</label>
+      <input
+        type='password'
+        name='password'
+        id='password'
+        onChange={handleChange}
+      />
+      <button type='submit'>Login</button>
+      <button type='button' onClick={() => logout()}>
+        Logout
+      </button>
+      {error && <p style={{color: 'white'}}>{error}</p>}
+    </form>
+  )
 }
