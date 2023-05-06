9026a6c2a5b6cee7f227a21875c1d50c48010c47
Rodrigo Escobar Lopez <ocralo@gmail.com>
2023-05-06 04:01:52 -0500

    feat: add auth context and functionality (#9)

--- src/pages/publics/SingUp/index.tsx
+++ src/pages/publics/SingUp/index.tsx
@@ -1,3 +1,42 @@
-export default function SingUp() {
-  return <div>SingUp</div>
+import {useState} from 'react'
+import {useAuth} from '@context/AuthContext/hooks'
+import {useNavigate} from 'react-router-dom'
+
+export default function SingUp(): JSX.Element {
+  const navigate = useNavigate()
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  const {signUp, error} = useAuth()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await signUp(user.email, user.password)
+    navigate('/home')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='email'>Email</label>
+      <input type='email' name='email' id='email' onChange={handleChange} />
+      <label htmlFor='password'>Password</label>
+      <input
+        type='password'
+        name='password'
+        id='password'
+        onChange={handleChange}
+      />
+      <button type='submit'>Login</button>
+      {error && <p style={{color: 'white'}}>{error}</p>}
+    </form>
+  )
 }
