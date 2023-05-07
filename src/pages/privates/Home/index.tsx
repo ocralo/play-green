be81be78bc7598af78cd7495b39b040d7e790fec
Rodrigo Escobar Lopez <ocralo@gmail.com>
2023-05-07 18:51:11 -0500

    feat: add use context sport in home page (#14)

--- src/pages/privates/Home/index.tsx
+++ src/pages/privates/Home/index.tsx
@@ -1,3 +1,16 @@
+import useSport from '@context/SportContext/hooks'
+
 export default function Home() {
-  return <div>Home</div>
+  const {getSports, sports} = useSport()
+  const handleRequest = async () => {
+    await getSports()
+  }
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={handleRequest}>Get All sports</button>
+      <code>{JSON.stringify(sports)}</code>
+    </div>
+  )
 }
