0f711033f0053934dcca19d3c749e7e3f5af1c9a
Rodrigo Escobar Lopez <ocralo@gmail.com>
2023-05-08 23:44:26 -0500

    feat: add styles and components in sing up (#20)
    * feat: add layout
    
    * feat: add components and layout

--- src/pages/publics/SingUp/index.tsx
+++ src/pages/publics/SingUp/index.tsx
@@ -1,6 +1,14 @@
 import {useState} from 'react'
-import {useAuth} from '@context/AuthContext/hooks'
 import {useNavigate} from 'react-router-dom'
+
+import {useAuth} from '@context/AuthContext/hooks'
+
+import FormInput from '@components/FormInput'
+import ErrorText from '@components/ErrorText'
+import Button from '@components/Button'
+import Form from '@components/Form'
+
+import {SingUpLayout} from './layout'
 
 export default function SingUp(): JSX.Element {
   const navigate = useNavigate()
@@ -9,34 +17,64 @@ export default function SingUp(): JSX.Element {
     password: '',
   })
 
-  const {signUp, error} = useAuth()
+  const {error, signUp} = useAuth()
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setUser({
       ...user,
       [e.target.name]: e.target.value,
     })
   }
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     e.preventDefault()
     await signUp(user.email, user.password)
     navigate('/home')
   }
 
+  const handleLogin = (): void => {
+    navigate('/login')
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='email'>Email</label>
-      <input type='email' name='email' id='email' onChange={handleChange} />
-      <label htmlFor='password'>Password</label>
-      <input
-        type='password'
-        name='password'
-        id='password'
-        onChange={handleChange}
-      />
-      <button type='submit'>Login</button>
-      {error && <p style={{color: 'white'}}>{error}</p>}
-    </form>
+    <SingUpLayout>
+      <h1 className='singup-title'>Create User</h1>
+      <h6 className='singup-description'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </h6>
+      <Form className='singup-form' onSubmit={handleSubmit}>
+        <FormInput
+          className='singup-input-user'
+          label='User'
+          name='email'
+          id='email'
+          type='email'
+          value={user.email}
+          onChange={handleChange}
+        />
+        <FormInput
+          className='singup-input-password'
+          label='Password'
+          name='password'
+          id='password'
+          type='password'
+          value={user.password}
+          onChange={handleChange}
+        />
+        {error && <ErrorText error={error} />}
+        <Button className='singup-submit-button' type='submit'>
+          SingUp
+        </Button>
+
+        <Button
+          type='button'
+          className='singup-login-button'
+          onClick={handleLogin}>
+          Login
+        </Button>
+      </Form>
+    </SingUpLayout>
   )
 }
